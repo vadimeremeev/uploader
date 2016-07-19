1756a0bb24a0e1cbb9994113263bf0d5231622b4
Vadim Eremeev <vadim@ensogo.com>
2016-07-19 19:15:46 +0800

    added models and migrations

---
+++ db/migrate/20160719100126_create_operations.rb
@@ -0,0 +1,15 @@
+class CreateOperations < ActiveRecord::Migration[5.0]
+  def change
+    create_table :operations do |t|
+      t.belongs_to :companies
+      t.string :invoice_num
+      t.date :invoice_date
+      t.date :open_date
+      t.float :amount
+      t.string :reporter
+      t.string :text
+      t.string :status
+      t.timestamps
+    end
+  end
+end
